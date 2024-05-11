b38c2d74abc85a3f2dd5e84da0749089599e0f52
dzwinok <dzwinka.stepanyuk@gmail.com>
2024-05-11 19:06:13 +0300

    waves

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,13 @@ import { Card,CardDescription,CardFooter,CardHeader } from "@/components/ui/card
 export default function Home() {
   return (
     <main className="px-[20%] flex  flex-col items-center justify-between p-24  ">
-    
-      <h2 className="text-tera-green text-[52px] font-[700] self-start px-2 py-5 ">Teraconsult</h2>
+        <WavyBackground className=" max-w-4xl mx-[100%]] pb-40">
+            <p className="z-40 text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+                Teraconsult
+            </p>
+        </WavyBackground>
+      {/*<h2 className="text-tera-green text-[52px] font-[700] self-start px-2 py-5 ">Teraconsult</h2>*/}
       <div className=" w-[100%] mr-1 mb-[3%] md:w-[42%] lg:w-[28%]">
-          {/*<WavyBackground className="max-w-4xl mx-[100%]] pb-40">
-              <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-                  Teraconsult
-              </p>
-              <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-                  Leverage the power of canvas to create a beautiful hero section
-              </p>
-          </WavyBackground>*/}
             
         <Card>
           <CardHeader>ІТ-КОНСАЛТИНГ
